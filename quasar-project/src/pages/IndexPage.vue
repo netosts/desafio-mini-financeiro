e5ec486a519128ad3d59c14b2080c6a2a90f6a1c
snufera <netostt91@gmail.com>
2023-10-16 13:35:40 -0300

    basic functionalities working, need to style components

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,55 +1,198 @@
 <template>
   <q-page padding>
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-form @submit="onSubmit">
+          <q-card-section>
+            <div class="text-h6">Categoria</div>
+            <q-select
+              dense
+              v-model="promptValues.label"
+              :options="manager.categories"
+              autofocus
+              lazy-rules
+              :rules="[
+                (val: string) =>
+                  (val && val.length > 0) ||
+                  'Por favor, selecione uma categoria',
+              ]"
+            />
+          </q-card-section>
+
+          <q-card-section>
+            <div class="text-h6">Cliente</div>
+            <q-select
+              dense
+              v-model="promptValues.cliente"
+              :options="manager.clients"
+              autofocus
+              lazy-rules
+              :rules="[
+                (val: string) =>
+                  (val && val.length > 0) || 'Por favor, selecione um cliente',
+              ]"
+            />
+          </q-card-section>
+
+          <q-card-section>
+            <div class="text-h6">{{ promptValues.text }}</div>
+            <q-input
+              dense
+              type="number"
+              v-model.number="promptValues.value"
+              autofocus
+              lazy-rules
+              :rules="[
+                (val: number) => (val && val > 0) || 'Por favor, informe um valor',
+              ]"
+            />
+          </q-card-section>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Cancelar" type="button" v-close-popup />
+            <q-btn
+              flat
+              :label="`Adicionar ${promptValues.text}`"
+              type="submit"
+            />
+          </q-card-actions>
+        </q-form>
+      </q-card>
+    </q-dialog>
+    <q-input
+      square
+      outlined
+      v-model="inputFilter"
+      @update:model-value="manager.findRecords(inputFilter)"
+      label="Pesquisar por Cliente"
+      style="margin-bottom: 20px"
+    />
+    <q-btn-dropdown color="info" label="Filtro">
+      <q-list>
+        <q-item clickable v-close-popup @click="manager.filterRecords('All')">
+          <q-item-section>
+            <q-item-label>Todos</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          v-for="(item, id) in manager.categories"
+          :key="id"
+          clickable
+          v-close-popup
+          @click="manager.filterRecords(item)"
+        >
+          <q-item-section>
+            <q-item-label>{{ item }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
     <q-table
       title="Entradas e Saídas"
       :rows="rows"
       :columns="columns"
       row-key="name"
     />
+    <div class="q-pa-md" style="padding-bottom: 30px">
+      <div class="q-mt-md">
+        <q-fab
+          v-model="addValueButton"
+          label="Adicionar"
+          label-position="left"
+          color="green"
+          icon="keyboard_arrow_right"
+          direction="right"
+        >
+          <q-fab-action
+            color="primary"
+            @click="promptEntry"
+            icon="add"
+            label="Entrada"
+          />
+          <q-fab-action
+            color="red"
+            @click="promptExit"
+            icon="horizontal_rule"
+            label="Saída"
+          />
+        </q-fab>
+      </div>
+    </div>
+    {{ total }}
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { useManager } from 'src/stores/manager';
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref, reactive } from 'vue';
+import { Record } from 'stores/manager';
 
 onMounted(() => {
   manager.init();
 });
 
+interface PromptValues {
+  text: string | undefined;
+  label: string | undefined;
+  cliente: string | undefined;
+  value: number | undefined;
+}
+
+const addValueButton = ref<boolean | undefined>(false);
+const prompt = ref<boolean | undefined>(false);
+const promptValues = reactive<PromptValues>({
+  text: undefined,
+  label: undefined,
+  cliente: undefined,
+  value: undefined,
+});
+
+const inputFilter = ref('');
+
 const manager = useManager();
+const total = computed(() => manager.total);
 const rows = computed<object[]>(() => manager.records);
 
 const columns = [
-  {
-    name: 'name',
-    required: true,
-    label: 'Id',
-    align: 'left',
-    field: (row: any) => row.id,
-    format: (val: any) => `${val}`,
-    sortable: true,
-  },
   {
     name: 'label',
     label: 'Categoria',
     field: 'label',
     sortable: true,
-    align: 'left',
+    align: 'center',
   },
   {
     name: 'cliente',
     label: 'Cliente',
     field: 'cliente',
     sortable: true,
-    align: 'left',
+    align: 'center',
   },
   {
     name: 'value',
     label: 'Valor',
     field: 'value',
     sortable: true,
-    align: 'left',
+    align: 'center',
   },
 ];
+
+async function onSubmit() {
+  if (promptValues.text === 'Saída' && promptValues.value !== undefined) {
+    promptValues.value = promptValues.value * -1;
+  }
+  delete promptValues.text;
+  await manager.addRecord(promptValues as Record);
+  prompt.value = false;
+}
+
+function promptEntry() {
+  promptValues.text = 'Entrada';
+  prompt.value = true;
+}
+
+function promptExit() {
+  promptValues.text = 'Saída';
+  prompt.value = true;
+}
 </script>
--- quasar-project/src/stores/manager.ts
+++ quasar-project/src/stores/manager.ts
@@ -2,21 +2,45 @@ import { defineStore } from 'pinia';
 import { getValores, getCategorias, getClientes } from 'src/services/api/get';
 import { postRecord } from 'src/services/api/post';
 
-interface Record {
+export interface Record {
   label: string;
   cliente: string;
   value: number;
 }
 
+interface Categories {
+  id: number;
+  label: string;
+}
+
+interface Clients {
+  id: number;
+  cliente: string;
+}
+
+const loadCategories = async () => {
+  const gettedCategories = await getCategorias();
+  const categoriesLabels = gettedCategories.map(
+    (item: Categories) => item.label
+  );
+  return categoriesLabels ? categoriesLabels : [];
+};
+const loadClients = async () => {
+  const gettedClients = await getClientes();
+  const clientsLabels = gettedClients.map((item: Clients) => item.cliente);
+  return clientsLabels ? clientsLabels : [];
+};
+
 export const useManager = defineStore({
   id: 'manager',
   state: () => ({
     records: [] as Record[],
+    categories: [] as string[],
+    clients: [] as string[],
   }),
   getters: {
     total(): number {
       return this.records.reduce((sumTotal, record) => {
-        console.log(record);
         return sumTotal + record.value;
       }, 0);
     },
@@ -24,10 +48,34 @@ export const useManager = defineStore({
   actions: {
     async init() {
       this.records = await getValores();
+      this.categories = await loadCategories();
+      this.clients = await loadClients();
     },
     async addRecord(inputValue: Record) {
       const response = await postRecord(inputValue);
       return response as Record;
+    },
+    async filterRecords(filter: string) {
+      const defaultRecords = await getValores();
+      if (filter === 'All') {
+        this.records = defaultRecords;
+        return;
+      }
+      const filteredRecords = defaultRecords.filter(
+        (record: Record) => record.label === filter
+      );
+      this.records = filteredRecords;
+    },
+    async findRecords(filter: string) {
+      const defaultRecords = await getValores();
+      if (filter === '') {
+        this.records = defaultRecords;
+        return;
+      }
+      const recordsFound = defaultRecords.filter((item: Record) =>
+        item.cliente.toLowerCase().includes(filter.toLowerCase())
+      );
+      this.records = recordsFound;
     },
   },
 });
